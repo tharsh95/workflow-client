3c0f691e534f70cf8f5408889012d8b59dbf32fa
Harsh Tiwari <harsh@kimo.ai>
2025-03-31 08:50:23 +0530

    Initial commit

--- src/contexts/WorkflowContext.jsx
+++ src/contexts/WorkflowContext.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 // Create an axios instance with a base URL
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api'
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 const WorkflowContext = createContext();
